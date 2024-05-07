0f89e75badf4f0b056e536965e004ef1690bf0f8
huycuongngo <cuong.nh@softenmind.com>
2024-05-07 20:09:57 +0700

    update 2

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { fetchProduct } from '../redux/services/productService'
 
 function ProductDetail() {
+  const dispatch = useDispatch()
+  const { id } = useParams()
+
+  useEffect(() => {
+    if (id) {
+      dispatch(fetchProduct(id))
+    }
+  }, [id])
+
   return (
     <div>ProductDetail</div>
   )
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { fetchProducts } from '../redux/services/productService'
 
 function ProductList() {
-  return (
-    <div>ProductList</div>
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const { products } = useSelector(state => state.product)
+  console.log("product from store", products)
+
+  useEffect(() => {
+    dispatch(fetchProducts())
+  }, [])
+
+  return products.length === 0 ? (<div>Loading...</div>) : (
+    <div style={{
+      display: 'grid',
+      gridTemplateColumns: '1fr 1fr 1fr 1fr',
+      gap: 20
+    }}>
+      {
+        products.map((product, index) => {
+          const { id, description, image, title, price } = product
+          return (
+            <div onClick={() => navigate(`/${id}`)} key={index} style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              gap: 10,
+              border: '1px solid gray',
+              borderRadius: 10,
+              cursor: 'pointer',
+              padding: 10
+            }}>
+              <img src={image} alt="" style={{
+                width: 200,
+                height: 200,
+              }} />
+              <h2>{title}</h2>
+              <p>{price} $</p>
+              <p>{description}</p>
+            </div>
+          )
+        })
+      }
+    </div>
   )
 }
 
